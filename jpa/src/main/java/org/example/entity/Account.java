0db1c91d43555114397995f5c065dc269a78f674
Noa <n.scelles974@gmail.com>
2025-05-27 11:30:15 +0200

    init jpa

---
+++ jpa/src/main/java/org/example/entity/Account.java
@@ -0,0 +1,28 @@
+package org.example.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private double amount;
+    private String customerName;
+    private String accountNumber;
+}
