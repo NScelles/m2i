cfd43b9808e72d7906a979e3e623a5f6901a13c6
Noa <n.scelles974@gmail.com>
2025-05-27 11:38:36 +0200

    init jpa

--- jpa/src/main/java/org/example/Main.java
+++ jpa/src/main/java/org/example/Main.java
@@ -1,11 +1,12 @@
 package org.example;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory ref = Persistence.createEntityManagerFactory("demo_jpa");
-        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa_demo");
+        EntityManager em = emf.createEntityManager();
     }
 }
