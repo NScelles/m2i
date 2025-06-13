5bc183c1321a2abce9783467496a4cc8514cb796
Noa <n.scelles974@gmail.com>
2025-06-13 16:35:57 +0200

    Modif Update, Delete Route to return response

--- jakarta/exericisesix/src/main/java/org/example/exericisesix/resources/CarResources.java
+++ jakarta/exericisesix/src/main/java/org/example/exericisesix/resources/CarResources.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.example.exericisesix.models.Car;
 import org.example.exericisesix.services.CarService;
 import java.util.List;
@@ -43,20 +44,43 @@ public class CarResources {
         return carService.get(car.getId());
     }
 
-    @POST
+//    @PUT
+//    @Path("/update")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public Car update(Car car) {
+//        carService.update(car.getId(),car);
+//        return carService.get(car.getId());
+//    }
+
+    @PUT
     @Path("/update")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Car update(Car car) {
-        carService.update(car.getId(),car);
-        return carService.get(car.getId());
+    public Response update(Car car) {
+        Car returnedCar = carService.update(car.getId(),car);
+        if (returnedCar != null) {
+            return Response.ok(returnedCar).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
     }
 
+//    @DELETE
+//    @Path("/delete/{id}")
+//    public int delete(@PathParam("id") int id){
+//        carService.delete(id);
+//        return id;
+//    }
+
     @DELETE
-    @Path("/delete/{id}")
-    public int delete(@PathParam("id") int id){
-        carService.delete(id);
-        return id;
+    @Path("delete/{id}")
+    public Response delete(@PathParam("id") int id){
+        if(carService.delete(id)){
+            return Response.ok().build();
+        }else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
     }
 
 }
