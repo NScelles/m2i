62457ffca076a4aa1f56cecbf1300d3057ccc94e
Noa <n.scelles974@gmail.com>
2025-06-13 14:02:39 +0200

    delete setCars Route

--- jakarta/exericisesix/src/main/java/org/example/exericisesix/resources/CarResources.java
+++ jakarta/exericisesix/src/main/java/org/example/exericisesix/resources/CarResources.java
@@ -21,19 +21,6 @@ public class CarResources {
     }
 
     @GET
-    @Path("/setCars")
-    @Produces(MediaType.APPLICATION_JSON)
-    public String setCars() {
-        Car car1 = new Car(1,"marque1",2020,"blue");
-        Car car2 = new Car(2,"marque2", 8520,"red");
-        Car car3 = new Car(3,"marque3", 128,"yellow");
-        carService.save(1,car1);
-        carService.save(2,car2);
-        carService.save(3,car3);
-        return "C'est bon";
-    }
-
-    @GET
     @Path("/get")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Car> get() {
