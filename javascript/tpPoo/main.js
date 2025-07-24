e5f7c0e61b7517fdb59c8de6565d44a8b964f4a3
Noa <n.scelles974@gmail.com>
2025-07-24 10:53:49 +0200

    Javascript TPpoo Bonus

--- javascript/tpPoo/main.js
+++ javascript/tpPoo/main.js
@@ -1,41 +1,99 @@
-import { createAnimal, getSpecificMethod } from "./models/animals/animalUtils.js";
+import { createAnimal, getImage, getSpecee, getSpecificMethod } from "./models/animals/animalUtils.js";
 import { Animal } from "./models/animals/animal.js";
 
 var zoo = [];
-const addAnimal = document.querySelector("form>button");
+const addAnimalButton = document.querySelector("form>button");
 const animalName = document.getElementById("animalName");
 const animalAge = document.getElementById("animalAge");
 const animalSpecee = document.getElementById("animalSpecee");
 const zooSection = document.getElementById("zoo");
 const animalAction = document.querySelector("section#animalAction>p");
 
-addAnimal.addEventListener("click",()=>{
+addAnimalButton.addEventListener("click",()=>{
     const animal = createAnimal(
         animalSpecee.value,
         animalName.value,
         animalAge.value
     );
-    zoo.push(animal);
-    createCard(animal,animalSpecee.value)
+    addAnimal(animal);
+    createCard(animal);
 });
 
-function createCard(animal,specee){
+function createCard(animal){
     const card = document.createElement("div");  
     const titleCard = document.createElement("h2");  
     const description = document.createElement("p");  
     const action = document.createElement("button");
+    const killButton = document.createElement("button");
+    const image = document.createElement("img");
 
     titleCard.textContent = animal.sePresenter();
-    description.textContent = animal.crier() + " Je suis un " + specee;
+    description.textContent = animal.crier() + " Je suis un " + getSpecee(animal);
     action.textContent = "action";
     action.addEventListener("click",()=>{
-        animalAction.textContent = getSpecificMethod(specee);
+        animalAction.textContent = getSpecificMethod(animal);
         
     });
-    
+    killButton.textContent = "Kill"
+    killButton.addEventListener("click",()=>{
+        removeAnimal(animal);
+        showZoo();
+    });
+    image.src=getImage(animal);
+
+    card.appendChild(image);
     card.appendChild(titleCard);
     card.appendChild(description);
     card.appendChild(action);
+    card.appendChild(killButton);
     zooSection.appendChild(card);
-    
 }
+
+function showZoo(){
+    zooSection.innerHTML="";
+    zoo.forEach((animal)=>{
+        createCard(animal)
+    });
+}
+
+function addAnimal(animal){
+    zoo.push(animal);
+    registerToLocalStorage();
+}
+
+function removeAnimal(animal){
+    zoo = zoo.filter(zooAnimal => animal!==zooAnimal);
+    registerToLocalStorage();
+}
+
+window.addEventListener("DOMContentLoaded",()=>{
+    getZooLocalStorage();
+    console.log(zoo);
+    showZoo();
+});
+
+function registerToLocalStorage(){
+    let zooJson = []; 
+    zoo.forEach((animalZoo)=>{
+        let animalObject = {
+            "animal":JSON.stringify(animalZoo),
+            "specee":getSpecee(animalZoo)
+        }
+        zooJson.push(animalObject);
+    })
+    localStorage.setItem("zoo",JSON.stringify(zooJson));
+}
+
+function getZooLocalStorage(){
+    let zooJson = JSON.parse(localStorage.getItem("zoo"));
+    console.log(zooJson);
+    
+    zooJson.forEach((animalZoo)=>{
+        let animalObject = JSON.parse( animalZoo["animal"]);
+        zoo.push(createAnimal(animalZoo["specee"],
+            animalObject["nom"],
+            animalObject["age"]
+        ));
+    })
+}
+
--- javascript/tpPoo/models/animals/animalUtils.js
+++ javascript/tpPoo/models/animals/animalUtils.js
@@ -13,13 +13,32 @@ export function createAnimal(type,name,age){
     }
 }
 
-export function getSpecificMethod(type){
-    switch(type){
-        case "Lion":
-            return Lion.chasser();
-        case "Parrot":
-            return Parrot.parler();
-        case "Snake":
-            return Snake.ramper();
-    }
+export function getSpecificMethod(animal){
+    if (animal instanceof Lion) {
+        return Lion.chasser();
+      } else if (animal instanceof Parrot) {
+        return Parrot.parler();
+      } else if (animal instanceof Snake) {
+        return Snake.ramper();
+      }
+}
+
+export function getSpecee(animal){
+    if (animal instanceof Lion) {
+        return "Lion";
+      } else if (animal instanceof Parrot) {
+        return "Parrot";
+      } else if (animal instanceof Snake) {
+        return "Snake";
+      }
+}
+
+export function getImage(animal){
+    if (animal instanceof Lion) {
+        return "https://upload.wikimedia.org/wikipedia/commons/7/73/Lion_waiting_in_Namibia.jpg";
+      } else if (animal instanceof Parrot) {
+        return "https://www.parrotworld.fr/fr/m_enhance/154?filename=perroquets-entrelaces&ext=jpg&style_name=gallery_image_large_1308_1088&h=544&w=654&rule=654w";
+      } else if (animal instanceof Snake) {
+        return "https://static.vecteezy.com/ti/vecteur-libre/t1/7270752-cartoon-vert-serpent-sur-fond-blanc-vectoriel.jpg";
+      }
 }
